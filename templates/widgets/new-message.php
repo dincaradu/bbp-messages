e966d034b14573919ca11a7ae7c43422ee16a7b7
elhardoum <elhardoum@outlook.com>
2017-08-07 15:32:31 +0100

    gettext domain string instead of constant reference

--- templates/widgets/new-message.php
+++ templates/widgets/new-message.php
@@ -33,7 +33,7 @@
 
         <li class="form-section">
             <?php wp_nonce_field('send_message', 'bbpm_nonce'); ?>
-            <input type="submit" value="<?php esc_attr_e('Send', BBP_MESSAGES_DOMAIN); ?>" />
+            <input type="submit" value="<?php esc_attr_e('Send', 'bbp-messages'); ?>" />
         </li>
     </ul>
 
